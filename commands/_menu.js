964fb978d624cb3d5d4456789e23cf628df58232
Bots.Business <git.sync@bots.business>
2022-06-22 01:08:37 +0000

    Bots.Business export for bot id: 691446

---
+++ commands/_menu.js
@@ -0,0 +1,11 @@
+/*CMD
+  command: /menu
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+  answer: သင်ခန်းစာများကို ရွေးချယ်ရန် 🔎
+  keyboard: Basic Course , Advance Course, Quest 
+  aliases: 
+CMD*/
+
