4e8e9527953807ee4dc5db8482686cc42307d628
Bots.Business <git.sync@bots.business>
2022-06-22 04:42:08 +0000

    Bots.Business export for bot id: 691446

---
+++ commands/_basic.js
@@ -0,0 +1,18 @@
+/*CMD
+  command: /basic
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: basic course
+CMD*/
+
+Bot.run({command:"/basiclessons"})
---
+++ commands/_basiclessons.js
@@ -0,0 +1,19 @@
+/*CMD
+  command: /basiclessons
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: basiclessons
+CMD*/
+
+Bot.sendInlineKeyboard([{title:"Lesson01", command:"basic01"},],"🐧 Linux Basic Lessons 🐧")
+
